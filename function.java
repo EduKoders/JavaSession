649dbe32ad596398a42fa6d4c4003abce2d6b82f
Ayesha Falak <60551252+ayeshafalak@users.noreply.github.com>
2021-06-27 19:24:41 +0530

    Create function.java

---
+++ function.java
@@ -0,0 +1,15 @@
+public class Main {
+    
+    public static int addNums(int a,int b)
+    {
+        int sum=a+b;
+        return sum;
+    }
+    public static void main() {
+        
+        int a=10;
+        int b=10;
+        int c = addNums(a,b);
+        System.out.println(c);
+    }
+}
